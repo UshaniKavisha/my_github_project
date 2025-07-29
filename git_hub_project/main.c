85aea00e738a6b55ab6d455c2c49cc915ae962e1
UshaniKavisha <as20240494@sci.sjp.ac.lk>
2025-07-29 05:46:41 +0530

    Add files via upload

---
+++ git_hub_project/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");
+    printf("Hello university of j'pura\n");
+    printf("Hello 1st year student\n");
+    printf("Hello kavisha\n");
+    printf("Hello software development fundamental \n");
+
+    return 0;
+}
